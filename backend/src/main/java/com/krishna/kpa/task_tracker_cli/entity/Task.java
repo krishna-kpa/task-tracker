fa05d3303d5726eda810741cbb1c526b3ca8d45a
Krishna Prasad A <kpa.krishnaprasada@gmail.com>
2025-03-08 02:54:09 +0530

    entity structure added

--- backend/src/main/java/com/krishna/kpa/task_tracker_cli/entity/Task.java
+++ backend/src/main/java/com/krishna/kpa/task_tracker_cli/entity/Task.java
@@ -1,4 +1,43 @@
 package com.krishna.kpa.task_tracker_cli.entity;
 
+import com.krishna.kpa.task_tracker_cli.enums.TaskStatus;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+/**
+ * Entity representing the task.
+ *
+ * @author Krishna Prasad A
+ */
+@Getter
+@Setter
 public class Task {
+
+    @Positive(message = "id of the task needs to be positive")
+    private Integer id;
+
+    @Size(min = 1, max = 100, message = "name size should be between 1 and 100")
+    @NotBlank(message = "name cannot be blank")
+    private String name;
+
+    @Size(min = 1, max = 255, message = "description size should be between 1 and 100")
+    @NotBlank(message = "name cannot be blank")
+    private String description;
+
+    @Future(message = "alert time needs to be future")
+    private LocalDateTime alertTime;
+
+    @NotNull(message = "status value cannot be null")
+    private TaskStatus status;
+
+    @LastModifiedDate
+    private LocalDateTime lastUpdatedTime;
+
+    @CreatedDate
+    private LocalDateTime createdTime;
 }
--- backend/src/main/java/com/krishna/kpa/task_tracker_cli/entity/User.java
+++ backend/src/main/java/com/krishna/kpa/task_tracker_cli/entity/User.java
@@ -1,4 +1,42 @@
 package com.krishna.kpa.task_tracker_cli.entity;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+/**
+ * Entity representing the user
+ *
+ * @author Krishna Prasad A
+ */
+@Getter
+@Setter
 public class User {
+
+    @Positive(message = "id of the task needs to be positive")
+    private Integer id;
+
+    @Size(min = 1, max = 100, message = "name size should be between 1 and 100")
+    @NotBlank(message = "name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "password value cannot be blank")
+    private String password;
+
+    @NotBlank(message = "phone number value cannot be blank")
+    @Pattern(regexp = "^(?:\\+91|91|0)?[789]\\d{9}$", message = "phone number pattern not matching")
+    private String phoneNumber;
+
+    @LastModifiedDate
+    private LocalDateTime lastUpdatedTime;
+
+    @CreatedDate
+    private LocalDateTime createdTime;
 }
